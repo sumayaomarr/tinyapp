1bca5ccfc12cdb7ffc8eeaa3b2326ff9922a774f
sumaya <sumayaomarr@gmail.com>
2021-12-07 22:44:05 -0500

    update

--- express_server.js
+++ express_server.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
 
-Fardosa:
+const cookieParser = require('cookie-parser');
+
 function generateRandomString() {
   let result = "";
 
@@ -26,6 +27,9 @@ const urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(cookieParser())
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -40,15 +44,25 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+     urls: urlDatabase,
+     username: req.cookies["username"]
+    };
+    console.log(req.cookies)
   res.render("urls_index", templateVars);
 });
+
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+   };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],
+  username: req.cookies["username"]
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -88,6 +102,21 @@ app.post("/urls/:shortURL/delete", (req, res) => {
   res.redirect(`/urls/${shortURL}`)
 
 });
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie('username',username); 
+  res.redirect(`/urls/`)
+
+});
+
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username'); 
+  res.redirect(`/urls/`)
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
